5dd19fecde9bcce15d65829f9371244bf9feeb55
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-04-23 08:15:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
    sort
   end 
   
-  def self.create_from_filename(filename)
+  def self.new_from_filename(filename)
     result = self.new_from_filename(filename)
     song = self.create
     song.name = result.name
