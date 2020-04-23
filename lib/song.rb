c842b94288da2e6ea6038e92102a88b6644f853d
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-04-23 07:56:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,30 @@ class Song
   def self.all
     @@all
   end
-
+  
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song = self.new 
+    self.all << song
+    song
+  end 
+  
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name 
+    song
+  end 
+  
+  def self.create_by_name(name)
+    song = self.new 
+    song.name = name 
+    self.all << song 
+    song 
+  end 
+  
+  
+  
 end
