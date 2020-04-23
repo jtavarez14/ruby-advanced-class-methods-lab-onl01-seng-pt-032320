4a1ab2b5d7c46bdd1be048307c6835561823b6c2
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-04-23 08:04:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ class Song
     @@all.find{|title| title.name == name}
   end 
   
-  def self.find_or_create_by_name
+  def self.find_or_create_by_name(name)
     result = self.find_by_name(name)
     if result  
       result 
