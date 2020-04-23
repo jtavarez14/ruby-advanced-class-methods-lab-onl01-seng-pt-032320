4f23d83e4162f64ace86079561353f5d70b05a05
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-04-23 08:16:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,10 +48,11 @@ class Song
   end 
   
   def self.new_from_filename(filename)
-    result = self.new_from_filename(filename)
-    song = self.create
-    song.name = result.name
-    song.artist_name = result.artist_name
+    song_array = filename.split(" - ")
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
     song
   end
   
